c9185336137268f59e16b689432102c859507788
silasjr <jose.silasjr.bb@gmail.com>
2016-12-07 19:29:20 +0000

    retirado bug

--- src/main/java/br/com/caelum/argentum/bean/ArgentumBean.java
+++ src/main/java/br/com/caelum/argentum/bean/ArgentumBean.java
@@ -38,7 +38,6 @@ public class ArgentumBean implements Serializable {
 	public ArgentumBean() {
 		this.negociacoes = new ClienteWebService().getNegociacoes();
 		geraGrafico();
-		var grafico = "Teste";
 	}
 
 	public void geraGrafico() {
